277cfc9bc4fa2124c93346b90ba108b9195e7c19
Marco Fernandez Pranno <mfernandezpranno@gmail.com>
2018-07-11 23:36:04 +0200

    Generating token, WIP

--- src/app.py
+++ src/app.py
@@ -1,11 +1,22 @@
-from flask import Flask, jsonify
+from flask import Flask, abort, jsonify, request
 from services.nodes import get_nodes
-from services.tokens import build_token
+from services.tokens import build_token, check_transaction, get_public_key
 
 import json
 import base64
 
 app = Flask(__name__)
+
+def init_app(app):
+   with open('public.pem','r') as f_pub:
+    with open('private.pem', 'r') as f_priv:
+        app.config.update(dict(
+                PUBLIC_KEY=f_priv.read(),
+                PRIVATE_KEY=f_pub.read(),
+            ))
+
+
+init_app(app)
 
 # Returns the list of nodes on the network
 @app.route("/nodes", methods=['GET'])
@@ -16,7 +27,7 @@ def get_nodes():
 # Returns the key the nodes will use to validate access tokens
 @app.route("/token_validation_key", methods=['GET'])
 def get_token_validation_key():
-    return "Token key"
+    return app.config['PUBLIC_KEY']
 
 # Endpoint for buying new access tokens
 @app.route("/token", methods=['POST'])
@@ -26,5 +37,17 @@ def post_token():
 # Returns access token for a given transaction (if this transaction has been validated)
 @app.route("/token", methods=['GET'])
 def get_token():
-    token = build_token()
-    return token.get('token')
+    tx_id = None
+    if 'TX_ID' in request.headers:
+        tx_id = request.headers.get('TX_ID')
+        if tx_id:
+            is_transaction_valid, tx_amount = check_transaction(tx_id)
+            if is_transaction_valid:
+                token = build_token(tx_amount)
+                return token.get('token')
+            else:
+                abort(401)
+        else:
+            abort(400)
+    else:
+        abort(401)
--- src/services/tokens.py
+++ src/services/tokens.py
@@ -1,23 +1,29 @@
+from flask import current_app as app
 from datetime import datetime
 from datetime import timedelta
+import time
 from Crypto.PublicKey import RSA
 
-def get_expire_date(days):
-    expire_date = datetime.now() + timedelta(days=days)
-    return expire_date.strftime('%m/%d/%Y')
+def get_expiration_timestamp(minutes):
+    expire_date = datetime.now() + timedelta(minutes=minutes)
+    return time.mktime(expire_date.timetuple())
 
-def _encrypt():
-    with open('public.pem','r') as f_pub:
-        with open('private.pem', 'r') as f_priv:
-            pub_key = RSA.importKey(f_pub.read())
-            priv_key = RSA.importKey(f_priv.read())
-            token = get_expire_date(1)
-            encrypted_token = pub_key.encrypt(token.encode('utf-8'), 32)
-            return encrypted_token
+def generate_token(minutes):
+    pub_key = RSA.importKey(app.config['PRIVATE_KEY'])
+    priv_key = RSA.importKey(app.config['PUBLIC_KEY']) 
+    expiration_date = get_expiration_timestamp(minutes)
+    encrypted_token = pub_key.encrypt(str(expiration_date).encode('utf-8'), 32)
+    return encrypted_token
 
-def build_token():
-    encrypted_token = _encrypt()[0]
+# Calculate time of access, build and encrypt token based on the price per unit of time
+def build_token(tx_amount):
+    encrypted_token = generate_token(10)[0]
     return dict(token=encrypted_token)
 
 
-
+# TODO: Connect to the wallet daemon and check if transaction is there
+def check_transaction(tx_id):
+    if tx_id:
+        return True, 10
+    else:
+        return False, None
