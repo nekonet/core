34436de3da85c09b261a9e3c2f04a5ee9d9fd2a2
Marco Fernandez Pranno <mfernandezpranno@gmail.com>
2018-07-11 21:46:42 +0200

    Basic endpoint declaration

--- src/app.py
+++ src/app.py
@@ -7,12 +7,24 @@ import base64
 
 app = Flask(__name__)
 
-@app.route("/get_token", methods=['GET'])
+# Returns the list of nodes on the network
+@app.route("/nodes", methods=['GET'])
+def get_nodes():
+    nodes = get_nodes()
+    return jsonify(nodes)
+
+# Returns the key the nodes will use to validate access tokens
+@app.route("/token_validation_key", methods=['GET'])
+def get_token_validation_key():
+    return "Token key"
+
+# Endpoint for buying new access tokens
+@app.route("/token", methods=['POST'])
+def post_token():
+    return "New token"
+
+# Returns access token for a given transaction (if this transaction has been validated)
+@app.route("/token", methods=['GET'])
 def get_token():
     token = build_token()
     return token.get('token')
-
-@app.route("/get_nodes", methods={'GET'})
-def get_nodes_list():
-    nodes = get_nodes()
-    return jsonify(nodes)
